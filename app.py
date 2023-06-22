c72d1f1f592b5fdd0821bc4d75d0cbea43595068
sreejith <squeeky.mouse@gmail.com>
2023-06-22 11:38:13 +0530

    add dynamic links in items, items page configs

--- app.py
+++ app.py
@@ -5,4 +5,5 @@
 # context in django,- key feature demo
 # create admin intr, add data
 # jinja for loop, display data on page
-# jinja if cond
+# jinja if/else cond, item avail in page
+# add dynamic links in items, items page configs
